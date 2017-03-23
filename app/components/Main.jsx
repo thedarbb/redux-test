111b4458748af3569c231e92380d8b12e44cd510
Dustin <dustin.sappier@bluespurs.com>
2017-03-23 16:02:21 -0300

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Main = (props) => {
+  return (
+    <div>
+      <Navigation/>
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
