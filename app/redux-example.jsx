132496975241ae1ceb327f835356185254c97bd5
Dustin <dustin.sappier@bluespurs.com>
2017-03-28 14:13:38 -0300

    add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,9 +4,25 @@ console.log('Starting redux example..');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
 
-  return state;
+  switch(action.type){
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      default:
+      return state;
+  }
 };
+
 var store = redux.createStore(reducer);
 var currentState = store.getState();
 
 console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Dustin'
+});
+
+console.log("Name should be Dustin", store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -4,10 +4,25 @@ var defaultState = {searchText: '', showCompleted: false, todos: []};
 
 var reducer = (state = defaultState, action) => {
 
-  return state;
+  switch(action.type){
+    case 'CHANGE_SEARCH_TEXT':
+    return {
+      ...state,
+      searchText: action.searchText
+    };
+    default:
+    return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 var currentState = store.getState();
 
 console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'test'
+});
+
+console.log("searchText should be test", store.getState());
