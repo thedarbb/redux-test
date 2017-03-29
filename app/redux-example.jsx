29b26f84332604fc6e3806a5feec925029f749bb
Dustin <dustin.sappier@bluespurs.com>
2017-03-29 11:05:36 -0300

    add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example..');
 
@@ -87,10 +88,54 @@ var removeMovie = (id) => {
   }
 };
 
+//Map reducer and action generators
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      }
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      }
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  }
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  }
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    console.log(res);
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q=';
+
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+};
+
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(
@@ -101,9 +146,13 @@ var store = redux.createStore(reducer, redux.compose(
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
-  document.getElementById('app').innerHTML = state.name;
-
   console.log('New state', store.getState());
+
+  if(state.map.isFetching){
+    document.getElementById('app').innerHTML = 'Loading...';
+  }else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View Your Location</a>';
+  }
 });
 
 // unsubscribe();
@@ -111,18 +160,16 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState', currentState);
 
+fetchLocation();
+
 store.dispatch(changeName('Dustin'));
 
 store.dispatch(addHobby('Video Games'));
-
 store.dispatch(addHobby('Sleeping'));
-
 store.dispatch(removeHobby(2));
 
 store.dispatch(changeName('Griffin'));
 
 store.dispatch(addMovie('Rogue One', 'Sci-Fi'));
-
 store.dispatch(addMovie('John Wick', 'Action'));
-
 store.dispatch(removeMovie(2));
