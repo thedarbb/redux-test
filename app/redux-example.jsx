148ebc4683cb0dc3a976230e06287aadc7195b0c
Dustin <dustin.sappier@bluespurs.com>
2017-03-29 11:42:51 -0300

    refactor file structure

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,148 +1,10 @@
 var redux = require('redux');
-var axios = require('axios');
 
 console.log('Starting redux example..');
 
-//Name reducer and action generators
-var nameReducer = (state = 'Anonymous', action) => {
-  switch(action.type){
-    case 'CHANGE_NAME':
-      return action.name;
-    default:
-      return state;
-  };
-};
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
 
-var changeName = (name) => {
-  return {
-    type: 'CHANGE_NAME',
-    name
-  }
-};
-
-//Hobbies reducer and action generators
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-  switch(action.type){
-    case 'ADD_HOBBY':
-      return [
-        ...state,
-        {
-          id: nextHobbyId++,
-          hobby: action.hobby
-        }
-      ];
-    case 'REMOVE_HOBBY':
-      return state.filter((hobby) => hobby.id !== action.id)
-    default:
-      return state;
-  }
-};
-
-var addHobby = (hobby) => {
-  return {
-    type: 'ADD_HOBBY',
-    hobby
-  }
-};
-
-var removeHobby = (id) => {
-  return {
-    type: 'REMOVE_HOBBY',
-    id
-  }
-};
-
-//Movies reducer and action generators
-var nextMovieId = 1;
-var moviesReducer = (state = [], action) => {
-  switch(action.type){
-    case 'ADD_MOVIE':
-      return [
-        ...state,
-        {
-          id: nextMovieId++,
-          title: action.title,
-          genre: action.genre
-        }
-      ];
-    case 'REMOVE_MOVIE':
-      return state.filter((movie) => movie.id !== action.id)
-    default:
-      return state;
-  }
-};
-
-var addMovie = (title, genre) => {
-  return {
-    type: 'ADD_MOVIE',
-    title,
-    genre
-  }
-};
-
-var removeMovie = (id) => {
-  return {
-    type: 'REMOVE_MOVIE',
-    id
-  }
-};
-
-//Map reducer and action generators
-var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-  switch (action.type) {
-    case 'START_LOCATION_FETCH':
-      return {
-        isFetching: true,
-        url: undefined
-      }
-    case 'COMPLETE_LOCATION_FETCH':
-      return {
-        isFetching: false,
-        url: action.url
-      }
-    default:
-      return state;
-  }
-};
-
-var startLocationFetch = () => {
-  return {
-    type: 'START_LOCATION_FETCH'
-  }
-};
-
-var completeLocationFetch = (url) => {
-  return {
-    type: 'COMPLETE_LOCATION_FETCH',
-    url
-  }
-};
-
-var fetchLocation = () => {
-  store.dispatch(startLocationFetch());
-
-  axios.get('http://ipinfo.io').then(function (res) {
-    console.log(res);
-    var loc = res.data.loc;
-    var baseUrl = 'http://maps.google.com?q=';
-
-    store.dispatch(completeLocationFetch(baseUrl + loc));
-  });
-};
-
-var reducer = redux.combineReducers({
-  name: nameReducer,
-  hobbies: hobbiesReducer,
-  movies: moviesReducer,
-  map: mapReducer
-});
-
-var store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-));
-
-//subscribe to changes
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
@@ -155,21 +17,19 @@ var unsubscribe = store.subscribe(() => {
   }
 });
 
-// unsubscribe();
-
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-fetchLocation();
+store.dispatch(actions.fetchLocation());
 
-store.dispatch(changeName('Dustin'));
+store.dispatch(actions.changeName('Dustin'));
 
-store.dispatch(addHobby('Video Games'));
-store.dispatch(addHobby('Sleeping'));
-store.dispatch(removeHobby(2));
+store.dispatch(actions.addHobby('Video Games'));
+store.dispatch(actions.addHobby('Sleeping'));
+store.dispatch(actions.removeHobby(2));
 
-store.dispatch(changeName('Griffin'));
+store.dispatch(actions.changeName('Griffin'));
 
-store.dispatch(addMovie('Rogue One', 'Sci-Fi'));
-store.dispatch(addMovie('John Wick', 'Action'));
-store.dispatch(removeMovie(2));
+store.dispatch(actions.addMovie('Rogue One', 'Sci-Fi'));
+store.dispatch(actions.addMovie('John Wick', 'Action'));
+store.dispatch(actions.removeMovie(2));
